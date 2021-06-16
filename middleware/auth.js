2d471c95b11d4f3848098579f47c6752a2f129a8
simbankenya <pharesmuruthi@gmail.com>
2021-06-16 19:36:37 +0300

    second commit

---
+++ middleware/auth.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+function auth(){
+    const token = req.header('x-header-token');
+    if(!token){
+        res.status(400).send('Access dined. No token provided');
+    }
+
+    try {
+        const decoded = jwt.verify(token, config.get('jwtPrivateKey'));
+        req.user = decoded;
+        next()
+    } catch (ex) {
+        res.status(400).send('invalid token');
+    }
+}
--- routes/users.js
+++ routes/users.js
@@ -2,34 +2,45 @@ const express = require('express');
 const User = require('../models/user');
 const router = express.Router();
 const bcrypt = require('bcrypt');
-const _ = require('lodash')
+const _ = require('lodash');
+const salt = require('../services/salt');
+const jwt = require('jsonwebtoken');
+const config = require('config')
 
 
 router.get('/', async (req, res)=>{
     const userList = await User.find();
     if(userList) res.status(200).send(userList);
 });
+
 router.post('/add', async (req, res)=>{
     const user = new User(_.pick(req.body, ['email', 'name', 'phone', 'passwordHash', 'isAdmin', 'street', 'apartment', 'zip']))
-    const salt = await bcrypt.genSalt(10);
+    
 
     user.passwordHash = await bcrypt.hash(user.passwordHash, salt);
-    // const  user = new User({
-    //     name: req.body.name,
-    //     email: req.body.email,
-    //     phone: req.body.phone,
-    //     passwordHash: req.body.passwordHash,
-    //     isAdmin: req.body.isAdmin,
-    //     street: req.body.street,
-    //     apartment: req.body.apartment,
-    //     zip: req.body.zip
-    // });
-    
+
     const newUser = await user.save();
     if(newUser){
        res.status(200).send(newUser) 
        return;
     } 
+})
+
+//user to login
+router.post('/login', async (req, res) => {
+
+    const user = await User.findOne({email: req.body.email});
+    if(!user){
+        res.status(400).send('invalid email or password')
+    }
+    const validPassword = await bcrypt.compare(req.body.passwordHash, user.passwordHash);
+
+    if(!validPassword) {
+        res.status(400).send('invalid password or email');
+    }
+    const token = jwt.sign({userId: user.id}, config.get('jwtPrivateKey'))
+    res.header('x-auth-token', token)
+    res.send(_.pick(user, ['_id', "name", "email"]))
 })
 
 //list of users 
@@ -47,6 +58,7 @@ router.get('/:id', async (req, res)=> {
         res.status(404).send('user with the given id not found..')
         return;
     }
+    
     res.status(200).send(user)
 })
 module.exports = router;
